c74b77469f184abadbb54b61cbac08264c77e0d1
Nagender Singh <nagenderrajput54@gmail.com>
2023-03-20 17:19:40 +0530

    initialize commit

---
+++ config/allowedOrigins.js
@@ -0,0 +1,8 @@
+const allowedOrigins = [
+    'https://www.yoursite.com',
+    'http://127.0.0.1:3000',
+    'http://localhost:3000',
+	'http://192.168.1.40:3000',
+];
+
+export default allowedOrigins;
